fdda542d12ad20de3754ec6a3977380c2c210599
Utkin Denis <stolen.youth@yandex.ru>
2022-08-12 19:22:55 +0700

    feat: deployment

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://snappy-chatapp-dacorm.herokuapp.com";
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const setAvatarRoute = `${host}/api/auth/setAvatar`
